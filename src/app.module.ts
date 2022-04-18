873e64ae3c2959ae82a9e70886290f8e679e23c2
CaseyMartin23 <cm232311@gmail.com>
2022-04-18 07:21:09 +0000

    Removed static file server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -18,9 +17,6 @@ import { WriteUpModule } from './write-up/write-up.module';
       ssl: true,
       useUnifiedTopology: true,
       useNewUrlParser: true,
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'admin-portal', 'build'),
     }),
     UserModule,
     AuthModule,
